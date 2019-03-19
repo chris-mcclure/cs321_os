fae43c01e865a2e5f58b0ba055ae9eca983f097c
Chris McClure <chrismcclure2010@gmail.com>
2019-03-19 10:41:41 -0800

    adding project folder with some working c code

---
+++ project/dumpelf.c
@@ -0,0 +1,54 @@
+#include <libelf.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+int main(int argc, char *argv[]){
+	/****** MAKING SURE ELF VERSION IS UP TO DATE *****/
+	if(elf_version(EV_CURRENT) == EV_NONE){
+		printf("YOU SUCK.\n");
+	}
+	
+	Elf *elf;
+	int fp = open(argv[1], O_RDONLY);
+	size_t index;
+	if(*argv[2] == 'd'){
+		printf("you're right\n");
+		index = 23;
+	}
+	else if(*argv[2] == 't'){
+		printf("you're not right\n");
+		index = 14;
+	}
+	/****** MAKE SURE THE ELF HAS THE MAGIC HEADER ******/
+	elf = elf_begin(fp, ELF_C_READ, NULL);
+	char *ehdr_ident = NULL;
+	ehdr_ident = elf_getident(elf, NULL);
+
+	if(ehdr_ident[0] == '\x7f' &&
+	   ehdr_ident[1] == '\x45' &&	//'E'
+	   ehdr_ident[2] == '\x4C' &&	//'L'
+	   ehdr_ident[3] == '\x46') {	//'F'
+		printf("this is a valid elf object file!\n");
+	}
+	
+	/*PRINTING OUT THE NUMBER OF SECTIONS ACCORDING TO THE HEADER*/
+	Elf64_Ehdr *eh64;
+	eh64 = elf64_getehdr(elf);
+	int shnum = eh64->e_shnum;
+	printf("number of sections: %d\n", shnum);
+
+	/**********************************************************/
+	/* This prints the correct size of the section in bytes!! */
+	Elf_Scn * curr = elf_getscn(elf, index);
+
+	Elf64_Shdr *sh64;
+	sh64 = elf64_getshdr(curr);
+	printf("section size is: %d bytes\n", (int)sh64->sh_size);
+
+	/**********************************************************/
+	return 0;
+}
