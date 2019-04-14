cb53797a9d513c78a5745ea0c758d38b8101039c
Chris McClure <chrismcclure2010@gmail.com>
2019-04-14 20:53:35 +0300

    Add files via upload
    added error checking to dumpelf.c

--- project/dumpelf.c
+++ project/dumpelf.c
@@ -1,6 +1,7 @@
-// Chris McClure
-// CS321 Rough Draft
-// Purpose: Prints the size of an Elf file that is passed to it.
+//Chris McClure
+//CS321 Project
+//Last revised: 4/14/19
+//Displays the side of sections.text and .data
 
 #include <libelf.h>
 #include <fcntl.h>
@@ -10,20 +11,30 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 
+size_t getSection(char);
+void printSize(Elf *, size_t);
+void initChecks(int, char *[]);
+
 int main(int argc, char *argv[]){
+	
+	initChecks(argc, argv); // make sure args are in correct format
+	
 	/****** MAKING SURE ELF VERSION IS UP TO DATE *****/
 	if(elf_version(EV_CURRENT) == EV_NONE){
-		printf("NOT CURRENT.\n");
-	}
-	
+		printf("Incorrect ELF version.\n");
+		exit(0);
+	} 
 	Elf *elf;
 	int fp = open(argv[1], O_RDONLY);
 
 	/****** MAKE SURE THE ELF HAS THE MAGIC HEADER ******/
 	elf = elf_begin(fp, ELF_C_READ, NULL);
+	if(!elf){
+		printf("didn't work!\n");
+		exit(0);
+	}
 	char *ehdr_ident = NULL;
 	ehdr_ident = elf_getident(elf, NULL);
-
 	if(ehdr_ident[0] == '\x7f' &&
 	   ehdr_ident[1] == '\x45' &&	//'E'
 	   ehdr_ident[2] == '\x4C' &&	//'L'
@@ -39,22 +50,57 @@ int main(int argc, char *argv[]){
 
 	/**********************************************************/
 	/* This prints the correct size of the section in bytes!! */
+	size_t num = getSection(*argv[2]);
+	printSize(elf, num);
+	/**********************************************************/
+	return 0;
+}
+
+// initChecks
+// function takes arguments passed into the command line.
+// if there isn't enough arguments passed to the command line, 
+// or the file passed isn't an executable, exit with a message. 
+void initChecks(int argc, char *argv[]){
+	struct stat sb;
+	if(stat(argv[1], &sb) == 0 && (sb.st_mode & S_IXUSR) == 0){
+		printf("was not passed an executable file. exiting.\n");
+		exit(0);
+	}
+	else if(argc < 3){
+		printf("expected 3 arguments. got %d, exiting.\n", argc);
+		exit(0);
+	}
+}
+
+
+// getSection
+// this function takes the paramater, sets and returns the corresponding index to main. otherwise the choice was invalid and program exits.
+size_t getSection(char choice){
+
+
+//find a way to get the index with out manually setting it
 	size_t index = 0;
-	if(*argv[2] == 'd'){
-		printf("section .data\n");
+	switch(choice){
+	case 'd':
+		printf("\nSECTION .DATA\n");
 		index = 23;
-	}
-	else if(*argv[2] == 't'){
-		printf("section .text\n");
+		break;
+	case 't':
+		printf("\nSECTION .TEXT\n");
 		index = 14;
+		break;
+	default:
+		printf("\nInvalid input, choices are t and d. rebuild and try again\n");
+		exit(0);
 	}
+	return index;
+}
 
-	Elf_Scn * curr = elf_getscn(elf, index);
-
+// printSize
+// function takes the ELF pointer as well as the index returned by getSection. gets the section header of the index and returns the size.
+void printSize(Elf *elf, size_t num){
+	Elf_Scn * curr = elf_getscn(elf, num);
 	Elf64_Shdr *sh64;
 	sh64 = elf64_getshdr(curr);
 	printf("section size is: %d bytes\n", (int)sh64->sh_size);
-
-	/**********************************************************/
-	return 0;
 }
